1a221fdaac8c1f1a0ae144140db948880be31f61
argowibowo <argo.wibowo1989@gmail.com>
2015-05-05 17:12:54 +0700

    Create NewRegionShippingCost.cs

---
+++ Marketplace.Interview/Marketplace.Interview.Business/Shipping/NewRegionShippingCost.cs
@@ -0,0 +1,15 @@
+namespace Marketplace.Interview.Business.Shipping
+{
+    public class NewRegionShippingCost
+    {
+        public string DestinationRegion { get; set; }
+        public decimal Amount { get; set; }
+
+        public static class Regions
+        {
+            public const string UK = "UK";
+            public const string Europe = "Europe";
+            public const string RestOfTheWorld = "RestOfTheWorld";
+        }
+    }
+}
